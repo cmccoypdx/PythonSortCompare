c17d7a9fa67476d2bb21aef9836b798d62c359b8
mick <mccoyc@cat.pdx.edu>
2018-11-18 11:21:12 -0800

    implement Selection Sort

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,1 +1,12 @@
-#code goes here
+
+
+def SSort(a):
+  b = []
+  while(len(a) > 0):
+    smallest = a[0]
+    for x in a:
+      if(x < smallest):
+        smallest = x
+    b.append(smallest)
+    a.remove(smallest)
+  return b
---
+++ main.py
@@ -0,0 +1,7 @@
+from SelectionSort import SSort
+
+a = [ 5, 29, -32, 47, 6 ]
+b = [ 4, 4, 4, 4, 4, 4, 5 ]
+
+print(SSort(a))
+print(SSort(b))
