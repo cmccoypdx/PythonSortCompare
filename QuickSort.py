2436f9f5a97fe1fd95a4e398fcbadbb26de65db6
mick <mccoyc@cat.pdx.edu>
2018-11-25 12:34:21 -0800

    implement quicksort

---
+++ QuickSort.py
@@ -0,0 +1,24 @@
+import random
+
+def HPart(a,l,r):
+  p = a[l]
+  i = l
+  j = r + 1
+  while(i < j):
+    i += 1
+    j -= 1
+    while(i < len(a) and a[i] < p):
+      i += 1
+    while(j > 1 and a[j] > p):
+      j -= 1
+    a[i], a[j] = a[j], a[i]
+
+  a[i], a[j] = a[j], a[i]
+  a[l], a[j] = a[j], a[l]
+  return j
+
+def QSort(a,l,r):
+  if(l < r):
+    s = HPart(a,l,r)
+    QSort(a,l,s-1)
+    QSort(a,s+1,r)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from SelectionSort import SSort
 from MergeSort import MSort
+from QuickSort import QSort
 
 a = [ 5, 29, -32, 47, 6 ]
 b = [ 4, 4, 4, 4, 4, 4, 5 ]
 
+QSort(a,0,len(a)-1)
+print(a)
+QSort(b,0,len(b)-1)
+print(b)
 print(SSort(a))
 print(SSort(b))
-print(MSort(a))
-print(MSort(b))
+#print(MSort(a))
+#print(MSort(b))
