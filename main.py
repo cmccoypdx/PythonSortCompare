2eb595160b4b7e34d8cc7c3f5843436d9ebbbc64
mick <mccoyc@cat.pdx.edu>
2018-11-26 18:06:28 -0800

    add sort verification

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import copy
+import random
 from SelectionSort import SSort
 from MergeSort import MSort
 from QuickSort import QSort
@@ -46,8 +47,10 @@ for _ in range(10):
   SmallPayloadArray.append(p[0:4])
 
 SDict = benchmark(SSort, SmallPayloadArray)
-MDict = benchmark(MSort, copy.deepcopy(PayloadArray))
-QDict = benchmark(QSort, copy.deepcopy(PayloadArray))
+MArray = copy.deepcopy(PayloadArray)
+QArray = copy.deepcopy(PayloadArray)
+MDict = benchmark(MSort, MArray)
+QDict = benchmark(QSort, QArray)
 
 SDict = {k : v / 10 for k, v in SDict.items()}
 MDict = {k : v / 10 for k, v in MDict.items()}
@@ -56,4 +59,28 @@ QDict = {k : v / 10 for k, v in QDict.items()}
 print(SDict)
 print(MDict)
 print(QDict)
-  
+
+arand = random.randint(0,9)
+print(arand)
+srand = random.randint(0,3)
+print(srand)
+brand = random.randint(0,6)
+print(brand)
+
+ss = SmallPayloadArray[arand][srand]
+ms = MArray[arand][brand]
+qs = QArray[arand][brand]
+
+if (all(ss[i] <= ss[i+1] for i in range(len(ss)-1))):
+  print("Sorted")
+else:
+  print("Not sorted")
+if (all(ms[i] <= ms[i+1] for i in range(len(ms)-1))):
+  print("Sorted")
+else:
+  print("Not sorted")
+if (all(qs[i] <= qs[i+1] for i in range(len(qs)-1))):
+  print("Sorted")
+else:
+  print("Not sorted")
+
